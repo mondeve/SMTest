db0c033316386c09b31cd0603a0ecdbc90dfb3cf
Raqhib <raqhib.ataillah@gmail.com>
2025-07-13 07:14:08 +0800

    add header request x-api-key: reqres-free-v1

--- app/src/main/java/id/co/mondo/suitmediatest/data/network/ApiConfig.kt
+++ app/src/main/java/id/co/mondo/suitmediatest/data/network/ApiConfig.kt
@@ -1,5 +1,6 @@
 package id.co.mondo.suitmediatest.data.network
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,11 +9,21 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
 
     private val BASE_URL = "https://reqres.in/api/"
+    private val API_KEY = "reqres-free-v1"
+
+    private val apiKeyInterceptor = Interceptor { chain ->
+        val originalRequest = chain.request()
+        val requestWithApiKey = originalRequest.newBuilder()
+            .addHeader("x-api-key", API_KEY)
+            .build()
+        chain.proceed(requestWithApiKey)
+    }
 
     fun getApiService(): ApiService {
         val loggingInterceptor =
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         val client = OkHttpClient.Builder()
+            .addInterceptor(apiKeyInterceptor)
             .addInterceptor(loggingInterceptor)
             .build()
         val retrofit = Retrofit.Builder()
